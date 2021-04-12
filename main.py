296905fbf276bf78387487a9728337ffe056b979
Alicia-krynne <alicemacrineadhiambo@gmail.com>
2021-04-12 17:02:41 +0300

    test  failed

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ def create_user(fname,lname,phone):
     '''
     new_user = user(fname,lname)
     return new_contact
-def save_contacts(contact):
+def save_credentials (credential):
     '''
-    Function to save contact
+    Function to save a credential
     '''
     credential.save_credentials()
-def del_contact(contact):
+def del_crdentials(credential):
     '''
-    Function to delete a contact
+    Function to delete a credential
     '''
     credential.delete_credentials()
 
@@ -26,12 +26,12 @@ def main():
             print('\n')
 
             while True:
-                    print("Use these short codes : cc - create a new contact, dc - display contacts, fc -find a contact, ex -exit the contact list ")
+                    print("Action: cd - create a new credentials, dc - delete credentials,sc -  save credential ")
 
                     short_code = input().lower()
 
-                    if short_code == 'cc':
-                            print("New Contact")
+                    if short_code == 'cd':
+                            print("New Credentials")
                             print("-"*10)
 
                             print ("First name ....")
@@ -43,50 +43,24 @@ def main():
                             print("Phone number ...")
                             p_number = input()
 
-                            print("Email address ...")
-                            e_address = input()
-
-
-                            save_contacts(create_contact(f_name,l_name,p_number,e_address)) # create and save new contact.
+                          
+                            save_credentials(create_user(f_name,l_name,p_number)) 
                             print ('\n')
-                            print(f"New Contact {f_name} {l_name} created")
+                            print(f"New User {f_name} {l_name} created")
                             print ('\n')
 
-                    elif short_code == 'dc':
-
-                            if display_contacts():
-                                    print("Here is a list of all your contacts")
-                                    print('\n')
-
-                                    for contact in display_contacts():
-                                            print(f"{contact.first_name} {contact.last_name} .....{contact.phone_number}")
-
-                                    print('\n')
+                   
                             else:
                                     print('\n')
-                                    print("You dont seem to have any contacts saved yet")
+                                    print(" no credentials available")
                                     print('\n')
 
-                    elif short_code == 'fc':
-
-                            print("Enter the number you want to search for")
-
-                            search_number = input()
-                            if check_existing_contacts(search_number):
-                                    search_contact = find_contact(search_number)
-                                    print(f"{search_contact.first_name} {search_contact.last_name}")
-                                    print('-' * 20)
-
-                                    print(f"Phone number.......{search_contact.phone_number}")
-                                    print(f"Email address.......{search_contact.email}")
-                            else:
-                                    print("That contact does not exist")
-
-                    elif short_code == "ex":
+                   
+                    elif short_code == "dc":
                             print("Bye .......")
                             break
                     else:
-                            print("I really didn't get that. Please use the short codes")
+                            print("delete  credentials")
 if __name__ == '__main__':
 
     main()
