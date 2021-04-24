293968708af30184c2d0e2e3d32144214a54e0da
Alicia-krynne <alicemacrineadhiambo@gmail.com>
2021-04-24 23:06:41 +0300

    redo  the  main.py

--- main.py
+++ main.py
@@ -55,115 +55,119 @@ def show_credentials(credentials):
 
 
 def main():
-  while True:
-		print("Hello Welcome to password-locker")
-		print('\n')
 
-		print(
-				"select short code: new user - nu: , log in : -lg , exit password locker : -ex")
-		short_code = input().lower()
-		print('\n')
 
-	if short_code == 'nu':
-		print("create username")
-		created_user_name = input()
+while True:
+	print("Hello Welcome to password-locker")
+	print('\n')
 
-		print("create password")
+	print(
+					"select short code: new user - nu: , log in : -lg , exit password locker : -ex")
+	short_code = input().lower()
+	print('\n')
+
+if short_code == 'nu':
+	print("create username")
+	created_user_name = input()
+
+	print("create password")
+	created_user_password = input()
+
+	print("confirm password")
+	confirm_user_pasword = input()
+
+	while confirm_user_pasword != created_user_password:
+		print("invalid!!! password did not match")
+		print("enter created password")
 		created_user_password = input()
-
 		print("confirm password")
 		confirm_user_pasword = input()
 
-		while confirm_user_pasword != created_user_password:
-				print("invalid!!! password did not match")
-				print("enter created password")
-				created_user_password = input()
-				print("confirm password")
-				confirm_user_pasword = input()
+	else:
+		print(
+						"congratulations{created_user_name}! account  creation  succesfull")
+		print('\n')
+		print("proceed to  log in")
+		print('\n')
+
+elif short_code == 'lg':
+	print("welcome!")
+	print("Enter Username")
+	created_user_name = input()
+
+	print("Enter password")
+	created_user_password = input()
+	print('\n')
+
+	while created_user_name != created_user_name or created_user_password != created_user_password:
+		print("invalid entry")
+		print("enter username")
+
+		created_user_name = input()
+
+		print("enter password")
+		created_user_password = input()
+		print('\n')
+	else:
+	print("log in  success")
+
+  while True:
+		print("what  do  you  want to do?:cn- create new credentials,sc- show credentials,fc- find credentials ")
+		short_code = input().lower()
+
+	if short_code == 'cn':  # created  and  saved the  credentials
+			print("new_credentials")
+			print("-"*10)
+
+			print("name....")
+			name = input()
+
+			print("website ...")
+			website = input()
+
+			print("password ...")
+			password = input()
+
+			save_credentials(new_credentials(name, website, password)) 
+			print('\n')
+			print(f" new Credentials {name} {website} {password} created")
+			print('\n')
+			
+	elif short_code == 'sc':
+			if show_credentials(credentials):
+					print("Here  you  go ^_^")
+					print('\n')
+
+					for credentials in show_credentials():
+							print(f"{credentials.name} {credentials.website} .....{credentials.password}")
+
+					print('\n')
 			else:
-					print(
-							"congratulations{created_user_name}! account  creation  succesfull")
 					print('\n')
-					print("proceed to  log in")
+					print("no  credentials, try  adding")
 					print('\n')
 
-        elif short_code == 'lg':
-            print("welcome!")
-            print("Enter Username")
-            created_user_name = input()
+	elif short_code == 'fc':
+			print("Enter the credentials you want to search for")
+			credentials_available = input()
+			if search_credential_by_website(search_credential_by_website):
+					search_credential_by_website = find_credentials(search_credential_by_website)
+					print(f"{find_credential.name} {find_credential.website} {find_credential.password}")
+					print('-' * 20)
 
-            print("Enter password")
-            created_user_password = input()
-            print('\n')
+					print(f"name .......{find_credential.name}")
+					print(f"website.......{find_credential.website}")
+					print(f"password.......{find_credential.password}")
 
-            while created_user_name != created_user_name or created_user_password != created_user_password:
-							print("invalid entry")
-							print("enter username")
+					else:
+							print("That  entry does not exist")
+    elif short_code == 'ex':
+        print("Bye  have  a  swell  day .......")
+        break
 
-							created_user_name = input()
-
-							print("enter password")
-							created_user_password = input()
-							print('\n')
-            else:
-              print("log in  success")
-
-            while True:
-                print("what  do  you  want to do?:cn- create new credentials,sc- show credentials,fc- find credentials ")
-                short_code = input().lower()
-
-                if short_code == 'cn':  # created  and  saved the  credentials
-                    print("new_credentials")
-                    print("-"*10)
-
-                    print("name....")
-                    name = input()
-
-                    print("website ...")
-                    website = input()
-
-                    print("password ...")
-                    password = input()
-
-                    save_credentials(new_credentials(name, website, password)) 
-                    print('\n')
-                    print(f" new Credentials {name} {website} {password} created")
-                    print('\n')
-                elif short_code == 'sc':
-                    if show_credentials(credentials):
-                        print("Here  you  go ^_^")
-                        print('\n')
-
-                        for credentials in show_credentials():
-                            print(f"{credentials.name} {credentials.website} .....{credentials.password}")
-
-                        print('\n')
-                    else:
-                        print('\n')
-                        print("no  credentials, try  adding")
-                        print('\n')
-
-                elif short_code == 'fc':
-                    print("Enter the credentials you want to search for")
-                    credentials_available = input()
-                    if search_credential_by_website(search_credential_by_website):
-                        search_credential_by_website = find_credentials(search_credential_by_website)
-                        print(f"{find_credential.name} {find_credential.website} {find_credential.password}")
-                        print('-' * 20)
-
-                        print(f"name .......{find_credential.name}")
-                        print(f"website.......{find_credential.website}")
-                        print(f"password.......{find_credential.password}")
-
-                        else:
-                            print("That  entry does not exist")
-        elif short_code == 'ex':
-            print("Bye  have  a  swell  day .......")
-            break
-
-    else:
-        print("I really didn't get that. Please use the short codes")
+else:
+    print("I really didn't get that. Please use the short codes")
 
 
 if __name__ == '__main__':
-    main()
+main()
